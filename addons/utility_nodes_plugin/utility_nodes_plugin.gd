bdec2913907e88f34dd370f37495291046695aa0
Jeffrey Kawai Cheung <CHEUNG.K.Jeffrey@gmail.com>
2025-02-13 13:40:48 -0800

    Refactor and rename into one NodeSceneLoader
    It's a NodeSceneLoader not a NodeProxy. The NodeProxy proxies functions,
    which this NodeSceneLoader does not do. Only one custom node is needed,
    while making it clear that the Node loads scenes (currently only in the
    editor scene). This is a better name.

--- addons/utility_nodes_plugin/utility_nodes_plugin.gd
+++ addons/utility_nodes_plugin/utility_nodes_plugin.gd
@@ -4,12 +4,8 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	add_custom_type("NodeProxy", "Node", preload("node_proxy.gd"), preload("res://icon.svg"))
-	add_custom_type("Node2DProxy", "Node2D", preload("node_proxy.gd"), preload("res://icon.svg"))
-	add_custom_type("Node3DProxy", "Node3D", preload("node_proxy.gd"), preload("res://icon.svg"))
-
+	add_custom_type("NodeSceneLoader", "Node", preload("node_scene_loader.gd"), preload("res://icon.svg"))
+	
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	remove_custom_type("NodeProxy")
-	remove_custom_type("Node2DProxy")
-	remove_custom_type("Node3DProxy")
+	remove_custom_type("NodeSceneLoader")
