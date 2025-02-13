abf8af088b019ed613a57ca68742dd45bb1427a8
Jeffrey Kawai Cheung <CHEUNG.K.Jeffrey@gmail.com>
2025-02-13 12:24:21 -0800

    Add proxy support for Node2D and Node3D
    This allows using one script to implement support for multiple different
    base nodes. The icons are a placeholder.
    
    To "attach" a script to these Nodes, you have to use extend script to
    prevent overwriting the node_proxy.gd script attached to the node.

--- addons/utility_nodes_plugin/node_proxy.gd
+++ addons/utility_nodes_plugin/node_proxy.gd
@@ -1,5 +1,4 @@
 @tool
-class_name NodeProxy
 extends Node
 
 @export_file var path: String
--- addons/utility_nodes_plugin/utility_nodes_plugin.gd
+++ addons/utility_nodes_plugin/utility_nodes_plugin.gd
@@ -4,9 +4,12 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	pass
-
+	add_custom_type("NodeProxy", "Node", preload("node_proxy.gd"), preload("res://icon.svg"))
+	add_custom_type("Node2DProxy", "Node2D", preload("node_proxy.gd"), preload("res://icon.svg"))
+	add_custom_type("Node3DProxy", "Node3D", preload("node_proxy.gd"), preload("res://icon.svg"))
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	pass
+	remove_custom_type("NodeProxy")
+	remove_custom_type("Node2DProxy")
+	remove_custom_type("Node3DProxy")
